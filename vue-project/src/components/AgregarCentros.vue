adf5565f4f51d294df080d2832e6e61915ef676e
Agustin Marasso Sheridan <82773623+Agustinms@users.noreply.github.com>
2022-06-20 13:31:33 -0300

    Agregado CSS

--- vue-project/src/components/AgregarCentros.vue
+++ vue-project/src/components/AgregarCentros.vue
@@ -1,72 +1,73 @@
 <template>
   <div>
-      <h2>Agregar Centros</h2>
-      <li v-for="centro in listaCentros" :key="centro.codigo"> 
-        {{ centro.nombre }} {{ centro.direccion}}
-        {{ centro.codigo }}
-        <button @click="borrarCentro(centro.codigo)">Borrar Centro</button>
-      </li>
-      <p>
-        Nombre <input type="text" v-model="centro.nombre">
-        Direccion <input type="text" v-model="centro.direccion">
-        Codigo <input type="text" v-model="centro.codigo">
-        <button @click="agregarCentro">Agregar Centro</button>
-      </p>
-      {{ mensajeError }}
+    <h2>Agregar Centros</h2>
+
+    <p v-for="centro in listaCentros" :key="centro.codigo">
+      {{ centro.nombre }} {{ centro.direccion }}
+      {{ centro.codigo }}
+      <button @click="borrarCentro(centro.codigo)">Borrar Centro</button>
+    </p>
+
+    <p>
+      Nombre <input type="text" v-model="centro.nombre" /> Direccion
+      <input type="text" v-model="centro.direccion" /> Codigo
+      <input type="text" v-model="centro.codigo" />
+      <button @click="agregarCentro">Agregar Centro</button>
+    </p>
+    {{ mensajeError }}
   </div>
 </template>
 
 <script>
-import apiCentro from '../services/apiCentro.js'
+import apiCentro from "../services/apiCentro.js";
 export default {
   data() {
     return {
-      centro:{direccion:"",nombre:'',codigo:""},
-      listaCentros : [],
-      mensajeError : ''
-    }
+      centro: { direccion: "", nombre: "", codigo: "" },
+      listaCentros: [],
+      mensajeError: "",
+    };
   },
   created: async function () {
-      try {        
-        const rta = await apiCentro.getListaCentros();
-        this.listaCentros = rta.data;
-      } catch( error ) {
-        console.log(error);
-        this.mensajeError = 'Se produjo un error en la conexion'
-      }
+    try {
+      const rta = await apiCentro.getListaCentros();
+      this.listaCentros = rta.data;
+    } catch (error) {
+      console.log(error);
+      this.mensajeError = "Se produjo un error en la conexion";
+    }
   },
   methods: {
     async agregarCentro() {
-      try {        
-        const obj = {...this.centro}
+      try {
+        const obj = { ...this.centro };
         const rta = await apiCentro.setcentro(obj);
         console.log(rta);
         this.listaCentros.push(obj);
-      } catch( error ) {
+      } catch (error) {
         console.log(error);
-        this.mensajeError = 'Se produjo un error en la conexion'
+        this.mensajeError = "Se produjo un error en la conexion";
       }
     },
     async borrarCentro(codigo) {
-      try {        
+      try {
         const rta = await apiCentro.deleteCentro(codigo);
         console.log(rta);
         console.log(codigo);
-        const listaCodigos = this.lista.map(e => {return e.codigo} )
+        const listaCodigos = this.lista.map((e) => {
+          return e.codigo;
+        });
         console.log(listaCodigos);
         const indice = listaCodigos.indexOf(codigo);
         console.log(indice);
-        this.lista.splice(indice,1);
-      } catch( error ) {
+        this.lista.splice(indice, 1);
+      } catch (error) {
         console.log(error);
-        this.mensajeError = 'Se produjo un error en la conexion'
+        this.mensajeError = "Se produjo un error en la conexion";
       }
-      
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
